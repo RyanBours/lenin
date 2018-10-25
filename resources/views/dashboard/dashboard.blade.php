e14b72a482faf0971dc16affd53b832205413431
Ryan Bours <ryan.bours@gmail.com>
2018-10-25 15:16:26 +0200

    Added authentication (pre)

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,5 +1,13 @@
 @extends('template') 
-@section('title', 'dashboard')
+@section('title', 'dashboard') 
 @section('content')
 <h1>dashboard</h1>
+<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+        document.getElementById('logout-form').submit();">
+    {{ __('Logout') }}
+</a>
+
+<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+    @csrf
+</form>
 @endsection
