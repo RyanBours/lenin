448953b5fd3611aa198eb38b453fd02904fa0dbc
Ryan Bours <ryan.bours@gmail.com>
2018-11-05 23:10:34 +0100

    Added belongsTo relations to Loan

--- app/Loan.php
+++ app/Loan.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Loan extends Model
@@ -13,4 +14,13 @@ class Loan extends Model
         // replace start & end with timestamp
         'start_date', 'end_date', 'expected_end_date', 'returned', 'user_id', 'item_id',
     ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function item() {
+        return $this->belongsTo(Item::class);
+    }
+
 }
