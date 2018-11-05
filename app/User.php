a6e9edfe07e14c8200178b2c41aa5b8e11a16818
Ryan Bours <ryan.bours@gmail.com>
2018-11-05 23:18:04 +0100

    Added function to model item & user to retrieve loans

--- app/User.php
+++ app/User.php
@@ -27,4 +27,8 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function loans() {
+        return $this->hasMany(Loan::class);
+    }
 }
