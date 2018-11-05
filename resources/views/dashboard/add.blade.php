21ac3531553d119843f3ae94492723ebff68ff24
unknown <stankikken@gmail.com>
2018-11-05 13:45:05 +0100

    dashboard en add page gestyled

--- resources/views/dashboard/add.blade.php
+++ resources/views/dashboard/add.blade.php
@@ -1,19 +1,34 @@
 @extends('template') 
 @section('title', 'add')
+@push('style')
+<style>
+  html,body {
+    height: 100%;
+  }
+
+  body {
+    background-image: url('/images/leeuwenborgh kleuren.png');
+    background-repeat: no-repeat;
+    background-size: 3em, auto;
+    background-position-x: 2em;
+  }
+</style>
+@endpush
 @section('content')
-<h1>add</h1>
+
+
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center align-items-center mt-5">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Add') }}</div>
+                <div class="card-header bg-leeuw" style="color:#eee">{{ __('Nieuw product toevoegen') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="/item/add/post">
                         @csrf
-
+                    <!--- toevoegen product naam -->
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Product naam') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
@@ -25,12 +40,43 @@
                                 @endif
                             </div>
                         </div>
+                    <!--- toevoegen NFC -->
+                        <div class="form-group row">
+                            <label for="NFC" class="col-md-4 col-form-label text-md-right">{{ __('NFC Code') }}</label>
 
+                            <div class="col-md-6">
+                                <input id="NFC" type="text" class="form-control{{ $errors->has('NFC') ? ' is-invalid' : '' }}" name="NFC" value="{{ old('NFC') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('NFC') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                    <!--- toevoegen Leenduur -->
+                        <div class="form-group row">
+                            <label for="Leenduur" class="col-md-4 col-form-label text-md-right">{{ __('Leenduur') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="Leenduur" type="text" class="form-control{{ $errors->has('Leenduur') ? ' is-invalid' : '' }}" name="Leenduur" value="{{ old('Leenduur') }}" required autofocus>
+
+                                @if ($errors->has('Leenduur'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('Leenduur') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <!-- TOEVOEG KNOP --->
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Add') }}
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-leeuw btn-rounded mb-4">
+                                    {{ __('Product toevoegen') }}
                                 </button>
+
+                        <!-- Terug Knop -->
+                                <a href="/dashboard" class="btn btn-leeuw btn-rounded mb-4">Annuleren</a>
                             </div>
                         </div>
                     </form>
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,13 +1,62 @@
 @extends('template') 
-@section('title', 'dashboard') 
-@section('content')
-<h1>dashboard</h1>
-<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-        document.getElementById('logout-form').submit();">
-    {{ __('Logout') }}
-</a>
+@section('title', 'dashboard')
+@push('style')
+<style>
+  html,body {
+    height: 100%;
+  }
 
-<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-    @csrf
-</form>
+  body {
+    background-image: url('/images/leeuwenborgh kleuren.png');
+    background-repeat: no-repeat;
+    background-size: 3em, auto;
+    background-position-x: 2em;
+  }
+
+
+  
+</style>
+@endpush 
+@section('content')
+<!-- Title -->
+
+<div class="container-fluid">
+  <div class="row justify-content-center">
+    <div class="col-10">
+      <h1>dashboard</h1>
+
+      <!-- Logout -->
+      <a class="btn btn-danger" href="{{ route('logout') }}" onclick="event.preventDefault();
+        document.getElementById('logout-form').submit();">
+        {{ __('Logout') }}
+      </a>
+
+      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+      </form>
+    </div>
+  </div>
+
+  <!-- Knoppen -->
+    <div class="row justify-content-center align-items-center mt-5">
+      <div class="col-5">
+        <a href="/item/leen" class="btn btn-leeuw btn-block" style="height:125px; margin:10px; text-align: center; line-height: 125px">Inscannen</a>
+      </div>
+
+      <div class="col-5">  
+        <a href="/item/my" class="btn btn-leeuw btn-block" style="height:125px; margin:10px; text-align: center; line-height: 125px">Mijn Items</a>
+      </div>
+
+
+      <div class="w-100"></div>
+
+      <div class="col-5">
+        <a href="/item/add" class="btn btn-leeuw btn-block" style="height:125px; margin:10px; text-align: center; line-height: 125px">Product Toevoegen</a>
+      </div>
+
+      <div class="col-5">
+        <a href="/item/beheer" class="btn btn-leeuw btn-block" style="height:125px; margin:10px; text-align: center; line-height: 125px">Alles Beheren</a>
+      </div>
+    </div>
+</div>
 @endsection
