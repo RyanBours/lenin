d895b8af4eeddbdbae81485e3fe527df09ba1317
Ryan Bours <ryan.bours@gmail.com>
2018-11-07 18:16:18 +0100

    Renamed /add to /item routing and added item editing

---
+++ app/Time.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+class Time
+{
+    public static function getTimeOfDayGreeting()
+    {
+        $currentTime = date('H');
+
+        // TODO(TGEnigma): L10n
+        if ( $currentTime >= 0 && $currentTime < 6 )
+        {
+            return "Goedenacht";
+        }
+        else if ( $currentTime >= 6 && $currentTime < 12 )
+        {
+            return "Goedemorgen";
+        }
+        else if ( $currentTime >= 12 && $currentTime < 18 )
+        {
+            return "Goedemiddag";
+        }
+        else if ( $currentTIme >= 18 && $currentTime < 0 )
+        {
+            return "Goedenavond";
+        }
+
+        assert(false);
+    }
+}
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -23,10 +23,14 @@
 @endpush 
 @section('content')
 <!-- Title -->
-<div class="container-fluid">
+
+<?php
+use App\Time; 
+?>
+<div class="container-fluid" style="overflow-y: hidden">
   <div class="row justify-content-center mt-5">
     <div class="col" align="center">
-      <h1>Hallo, {{Auth::user()->name}}</h1>
+      <h1>{{Time::getTimeOfDayGreeting()}}, {{Auth::user()->name}}</h1>
       <hr />
       <!-- Logout -->
       <a class="btn btn-danger" href="{{ route('logout') }}" onclick="event.preventDefault();
@@ -34,35 +38,35 @@
         {{ __('Uitloggen') }}
       </a>
       <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-          @csrf
-        </form>
+        @csrf
+      </form>
     </div>
   </div>
 
   <!-- Knoppen -->
   <div class="row justify-content-center mx-auto mt-5">
     <div class="col-3">
-      <a href="/return" class="btn btn-leeuw btn-block" style="height:125px; margin:10px; text-align: center; line-height: 125px">Terugbrengen</a>
+      <a href="/return" class="btn btn-leeuw btn-block" style="height:125px; margin:10px; text-align: center; line-height: 100px">Terugbrengen</a>
     </div>
   </div>
   
   <div class="row justify-content-center mx-auto mt-1">
     <div class="col-5">
-      <a href="/dashboard/leen" class="btn btn-leeuw btn-block" style="height:125px; margin:10px; text-align: center; line-height: 125px">Inscannen</a>
+      <a href="/dashboard/leen" class="btn btn-leeuw btn-block" style="height:125px; margin:10px; text-align: center; line-height: 100px">Inscannen</a>
     </div>
 
     <div class="col-5">  
-      <a href="/dashboard/my" class="btn btn-leeuw btn-block" style="height:125px; margin:10px; text-align: center; line-height: 125px">Mijn Items</a>
+      <a href="/dashboard/my" class="btn btn-leeuw btn-block" style="height:125px; margin:10px; text-align: center; line-height: 100px">Mijn Items</a>
     </div>
 
     @if (Auth::User()->permission_level > 0)
     <div class="w-100"></div>
     <div class="col-5">
-      <a href="/dashboard/item" class="btn btn-leeuw btn-block" style="height:125px; margin:10px; text-align: center; line-height: 125px">Product Toevoegen</a>
+      <a href="/dashboard/item" class="btn btn-leeuw btn-block" style="height:125px; margin:10px; text-align: center; line-height: 100px">Product Toevoegen</a>
     </div>
 
     <div class="col-5">
-      <a href="/dashboard/beheer" class="btn btn-leeuw btn-block" style="height:125px; margin:10px; text-align: center; line-height: 125px">Alles Beheren</a>
+      <a href="/dashboard/beheer" class="btn btn-leeuw btn-block" style="height:125px; margin:10px; text-align: center; line-height: 100px">Alles Beheren</a>
     </div>
     @endif
   </div>
