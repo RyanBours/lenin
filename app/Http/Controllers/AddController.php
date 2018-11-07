5a1f25907859ea87f8796c7f4a5fe01aee830b81
Ryan Bours <ryan.bours@gmail.com>
2018-11-07 10:58:37 +0100

    Added validation on the Add controller

--- app/Http/Controllers/AddController.php
+++ app/Http/Controllers/AddController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Item;
+use Validator;
 use App\Http\Controllers\Session;
 use Illuminate\Http\Request;
 
@@ -22,6 +23,18 @@ class AddController extends Controller {
     }
 
     public function post(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|unique:items|max:255',
+        ], [
+            'name.unique' => 'Name has already been used!'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/item/add')
+                        ->withErrors($validator)
+                        ->withInput();
+        }
+
         $item = new Item;
         $item->name = $request->name;
         $item->max_loan_duration = 0;
@@ -30,7 +43,11 @@ class AddController extends Controller {
         if ($succ) {
             $request->session()->flash('status', $item->name.' has been added!'); 
             $request->session()->flash('alert-class', 'alert-info'); 
+            return redirect('/item/add');
         }
-        return redirect('item/add');
+
+        $request->session()->flash('status', ' Somenthing went wrong'); 
+        $request->session()->flash('alert-class', 'alert-danger');
+        return redirect('/item/add');
     }
 }
