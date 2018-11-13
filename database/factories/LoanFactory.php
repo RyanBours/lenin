464fffefd8acaf474b274e43fa65104a8c524e2e
Ryan Bours <ryan.bours@gmail.com>
2018-11-13 11:07:00 +0100

    Changed LoanFactory data gets created based on, if loan is returned

--- database/factories/LoanFactory.php
+++ database/factories/LoanFactory.php
@@ -8,12 +8,13 @@ use Faker\Generator as Faker;
 
 $factory->define(Loan::class, function (Faker $faker) {
     $item_id = $faker->unique()->numberBetween(1, 150);
-    $end_date = Carbon::now();
-
+    $current_date = Carbon::now();
+    $end_date = Carbon::createFromFormat('Y-m-d H:i:s', $current_date)->addDays(Item::find($item_id)->max_loan_duration);
+    $returned = $faker->numberBetween(0, 1);
     return [
-        'end_date' => $end_date,
-        'expected_end_date' => Carbon::createFromFormat('Y-m-d H:i:s', $end_date)->addDays(Item::find($item_id)->max_loan_duration),
-        'returned' => $faker->numberBetween(0, 1),
+        'end_date' => $returned ? $end_date : null,
+        'expected_end_date' => $end_date,
+        'returned' => $returned,
         'user_id' => factory(User::class)->create()->id,
         'item_id' => $item_id
     ];
